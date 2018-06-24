6d4450136b3c8c2b9f1e842c6b7a79d7b708265c
parkersalterego <parkersalterego92@gmail.com>
2018-06-23 22:21:51 -0700

    login register and home complete(design)

--- frontEnd/src/app/components/nav/nav.component.ts
+++ frontEnd/src/app/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +6,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  navBarCount = 0;
+  navItems = [];
+
+  @ViewChild('barOne') barOne;
+  @ViewChild('barTwo') barTwo;
+  @ViewChild('barThree') barThree;
+  @ViewChild('navBar') navBar;
 
   constructor() { }
 
   ngOnInit() {
+    this.pushNavItems();
   }
+
+  onMenuClick() {
+    if (this.navBarCount === 0 ) {
+      this.navItems.forEach((item) => {
+        item.nativeElement.classList.add('animate');
+      });
+      this.barOne.nativeElement.classList.add('white');
+      this.barTwo.nativeElement.classList.add('white');
+      this.barThree.nativeElement.classList.add('white');
+      this.navBarCount++;
+    } else if (this.navBarCount % 2 === 1) {
+      this.navItems.forEach((item) => {
+        item.nativeElement.classList.remove('animate');
+        item.nativeElement.classList.add('animate-reverse');
+      });
+      this.barOne.nativeElement.classList.remove('white');
+      this.barTwo.nativeElement.classList.remove('white');
+      this.barThree.nativeElement.classList.remove('white');
+      this.navBarCount++;
+    } else {
+      this.navItems.forEach((item) => {
+        item.nativeElement.classList.add('animate');
+        item.nativeElement.classList.remove('animate-reverse');
+      });
+      this.barOne.nativeElement.classList.add('white');
+      this.barTwo.nativeElement.classList.add('white');
+      this.barThree.nativeElement.classList.add('white');
+      this.navBarCount--;
+    }
+  }
+
+  onListItemClick() {
+    this.navItems.forEach((item) => {
+      item.nativeElement.classList.remove('animate');
+      item.nativeElement.classList.add('animate-reverse');
+    });
+    this.navBarCount++;
+  }
+
+  pushNavItems() {
+    this.navItems.push(this.barOne);
+    this.navItems.push(this.barTwo);
+    this.navItems.push(this.barThree);
+    this.navItems.push(this.navBar);
+  }
+
 
 }
--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = require('express').Router();
 
+const ExampleController = require('./example.controller.js');
 const UserController = require('./user.controller');
 
+const exampleController = new ExampleController(router);
 const userController = new UserController(router);
 
 module.exports = router;
