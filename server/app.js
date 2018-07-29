8646ca6ac6979bc01d4d060bdc641ba790cf9e63
parkersalterego <parkersalterego92@gmail.com>
2018-07-29 15:01:45 -0700

    update connection

--- server/app.js
+++ server/app.js
@@ -52,12 +52,12 @@ app.use(cookieParser());
 
 // CORS Middleware
 app.use((req, res, next) => {
-    // var allowedOrigins = ['https://cablechanwedding.win', 'https://www.cablechanwedding.win'];
-    // var origin = req.headers.origin;
-    // if(allowedOrigins.indexOf(origin) > -1){
-    //      res.setHeader('Access-Control-Allow-Origin', origin);
-    // }
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    let allowedOrigins = ['https://cablechanwedding.win', 'https://www.cablechanwedding.win'];
+    let origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     next();
